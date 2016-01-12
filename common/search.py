24ccd15c5ec2a8d42c600099ff610548d6e91845
light.zhang <zhilight@gmail.com>
2016-01-13 00:23:30 +0800

    设想一种便利目录的方法

--- common/search.py
+++ common/search.py
@@ -5,8 +5,6 @@
 import os
 import os.path
 import time
-import hashlib
-import pickle
 
 
 def deep_search(path):
@@ -29,6 +27,16 @@ def deep_search2(path):
             dir_list.append(os.path.join(path, name))
     return dir_list, file_list
 
+def deep_search3(path):
+    dir_content = os.listdir(path)
+    dir_list = []
+    file_list = []
+    for name in dir_content:
+        if os.path.isfile(name):
+            print('%s is a common file.' % name)
+        elif os.path.isdir(name):
+            print('%s is a directory.' % name)
+
 
 def deep_search_list(dir_list):
     for dir in dir_list:
@@ -47,9 +55,5 @@ def get_hexdigest(filename):
     return md5_str
 
 if __name__ == '__main__':
-    filename = '/Users/light.zhang/log/slow.log'
-    start_time = time.clock()
-    md5_str = get_hexdigest(filename)
-    end_time = time.clock()
-    print('hash of %s: %s' % (filename, md5_str))
-    print('duration: %.4f seconds' % (end_time - start_time))
+    deep_search3('/')
+    print(os.listdir('/'))
